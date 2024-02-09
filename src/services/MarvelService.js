a7945cd7f9ef7ff6d366946b91a4104d89dcb20b
Daria Artiomenko <daridaraart@gmail.com>
2024-02-09 12:39:32 +0300

    ErrorBoundary component and loading more characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=f4649578ff806c4bfdc5998f18f3c34c';
+    _baseOffset = 210;
+    
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -12,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
     getCharacter = async (id) => {
